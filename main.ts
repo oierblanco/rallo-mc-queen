1553aac9c926a09ade48a89d784a1c1e67e066c2
oierblanco <93262346+oierblanco@users.noreply.github.com>
2022-03-16 14:25:01 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
+basic.pause(2000)
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
+basic.pause(500)
+DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 30)
+basic.pause(2000)
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
+basic.pause(500)
+basic.forever(function () {
+	
+})
